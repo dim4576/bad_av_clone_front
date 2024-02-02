d29067b0db35166f6fc44de9cb966372146caf57
admin <dim457676@gmail.com>
2024-02-02 18:19:12 +0300

    bolted normal!!!! login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,26 +1,24 @@
 import React, {useState} from "react";
 import ElevateAppBarUniversal from "./navUniversal";
-//import {useNavigate} from 'react-router-dom';
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
 
 const Content = ({setToken}) => {
+
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
+    const navigate = useNavigate();
+
+    async function handleSubmit(event) {
 
 
-    async function handleSubmit() {
+        event.preventDefault()
         const url = 'http://127.0.0.1:8000/api-token-auth/';
-        //axios.defaults.withCredentials = true
-        axios.defaults.timeout = 10000
-        //axios.defaults.headers.post["Content-Type"] = 'application/x-www-form-urlencoded'
-        axios.options(url).then((re) => {
-            console.log(re.headers, re.data)
-            //alert('options')
-        }).catch((err) => {
-            console.log(err)
-            alert('options with err, check console')
-        })
-        const config = {
+        axios.defaults.timeout = 500
+        let response = {}
+        let error = {}
+        let config = {
             method: 'post',
             maxBodyLength: Infinity,
             url: url,
@@ -30,31 +28,26 @@ const Content = ({setToken}) => {
             }
         }
 
+        response = await axios.request(config)
+        console.log('[befor read]', response, error)
+        localStorage.setItem('token', response.data.token);
+        setToken(response.data.token)
+        setPassword('')
+        setLogin('')
+        navigate('/login')
 
-        axios.request(config
-        ).then((response) => {
-            //alert('check console with pass')
-            localStorage.setItem('token', response.data.token);
-            setToken(response.data.token)
-            console.log(response.data)
-            //alert('check console with pass')
-        }).catch((err) => {
-                console.log(err);
-                alert('check console with fail')
-            }
-        )
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <label>
                 введите логин:
-                <input type="text" name="login" onChange={(e) => {
+                <input type="text" name="login" value={login} onChange={(e) => {
                     setLogin(e.target.value)
                 }}/>
                 <br/>
                 введите пароль:
-                <input type="password" name="password" onChange={(e) => {
+                <input type="password" name="password" value={password} onChange={(e) => {
                     setPassword(e.target.value)
                 }}/>
             </label>
--- src/card.jsx
+++ src/card.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,45 +7,62 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+//import axios from "axios";
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
 });
 
 export default function MediaCard(elem) {
-  //console.log("[in media card elem] --> ", elem)
-  const classes = useStyles();
+    //console.log("[in media card elem] --> ", elem)
+    const classes = useStyles();
+    let img = elem.photos[0];
 
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={elem.photos}
-          title={elem.brand}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {elem.model}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            price: {elem.price}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    /*let config = {
+        withCredentials: false,
+        maxBodyLength: Infinity,
+        method: 'get',
+        url: elem.photos[0],
+        headers: {}
+        //responseType: 'stream'
+    };
+
+    let response = await axios.request(config)
+    console.log(response)*/
+
+
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={img}
+                    title={elem.brand}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {elem.brand}
+                        <br/>
+                        {elem.model}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        price: {elem.priceUSD}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+                </Button>
+                <Button size="small" color="primary">
+                    Learn More
+                </Button>
+            </CardActions>
+        </Card>
+    );
 }
